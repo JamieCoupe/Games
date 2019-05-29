d35f8dd8630107ad222d904d43731d944511b20a
Jamie Coupe <Jamie.coupe@sky.com>
2019-05-29 13:30:10 +0100

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -11,60 +11,51 @@ logging.disable(logging.DEBUG)
 
 EASY_WORDS = ['can', 'tan', 'apple']
 MEDIUM_WORDS = ['medium', 'special', 'caravan']
-HARD_WORDS = ['Awkward', 'Bagpipes', 'BanjoBungler','Croquet','Crypt', 'Dwarves', 'Fervid', 'Fishhook', 'Fjord',
-              'Gazebo', 'Gypsy', 'Haiku', 'Haphazard', 'Hyphen', 'Ivory', 'Jazzy', 'Jiffy', 'Jinx', 'Jukebox', 'Kayak',
-              'Kiosk', 'Klutz', 'Memento', 'Mystify', 'Numbskull', 'Ostracize', 'Oxygen', 'Pajama', 'Phlegm', 'Pixel',
-              'Polka', 'Quad', 'Quip', 'Rhythmic', 'Rogue', 'Sphinx', 'Squawk', 'Swivel', 'Toady', 'Twelfth', 'Unzip',
-              'Waxy', 'Wildebeest', 'Yacht', 'Zealous','Zigzag', 'Zippy', 'Zombie']
+HARD_WORDS = ['whisper', 'worship', 'arcane']
 
 
-def read_word_list(filename):
-    with open(filename) as word_file:
-      return word_file.readlines()
-  
-  
 def blank_word(word_to_blank):
     blanked_word = ''
 
     for letter in enumerate(word_to_blank):
         blanked_word += '-'
 
-    game_variables = {'original': word_to_blank, 'blanked': blanked_word, 'counter': 0, 'letters': ''}
-    return game_variables
+    all_words = {'original': word_to_blank, 'blanked': blanked_word, 'counter': 0, 'letters': ''}
+    return all_words
 
 
-def blank_word_guess(game_variables, guess):
+def blank_word_guess(all_words, guess):
     logging.debug('Guess is ' + guess)
-    original_word = str(game_variables['original'])
-    guessed_word = str(game_variables['blanked'])
-    new_guessed_word = ''
-    counter = game_variables['counter']
+    original_word = str(all_words['original'])
+    guessed_word = str(all_words['blanked'])
+    new_guessed = ''
+    counter = all_words['counter']
     for letter_index in range(len(original_word)):
-        logging.debug('Letter index is {}'.format(str(letter_index))
+        logging.debug('Letter index is ' + str(letter_index))
         if original_word[letter_index] in guessed_word or original_word[letter_index] == guess:
             logging.debug('Letter is the guess or already guessed')
-            new_guessed_word  += original_word[letter_index]
+            new_guessed += original_word[letter_index]
         else:
             logging.debug('Letter is not a match to this letter moving onto the next')
-            new_guessed_word  += '-'
+            new_guessed += '-'
 
-    game_variables = {'original': original_word, 'blanked': new_guessed_word, 'counter': counter, 'letters': game_variables['letters']}
-    return game_variables
+    all_words = {'original': original_word, 'blanked': new_guessed, 'counter': counter, 'letters': all_words['letters']}
+    return all_words
 
 
-def guess_letter(game_variables, guess):
-    original_word = str(game_variables['original'])
+def guess_letter(words, guess):
+    original_word = str(words['original'])
     if guess in original_word:
         print('Guess %s was correct' % guess)
-        game_variables = blank_word_guess(game_variables, guess)
-    elif guess in game_variables['letters']:
+        words = blank_word_guess(words, guess)
+    elif guess in words['letters']:
         print('You have guessed this letter before')
     else:
         print('Guess %s was incorrect' % guess)
-        game_variables['counter'] = game_variables['counter'] + 1
-        game_variables['letters'] = game_variables['letters'] + ' ' + guess
-    logging.debug('The returned words are {}'.format(str(game_variables)
-    return game_variables
+        words['counter'] = words['counter'] + 1
+        words['letters'] = words['letters'] + ' ' + guess
+    logging.debug('The returned words are ' + str(words))
+    return words
 
 
 def print_gallows(man):
@@ -93,30 +84,32 @@ def get_word(difficulty):
         return get_hard_word()
 
 
+def get_word_list(filename):
+    with open(filename) as word_file:
+      return [word.rstrip('\n') for word in word_file]
+
+
 def get_easy_word():
     logging.debug('Getting an easy word')
-    # words = read_word_list('easy.txt')
-    words = EASY_WORDS
-    word = str(random.choice(words)).lower
-    logging.debug('Returning word {}'.format(word))
+    words = get_word_list('easy.txt')
+    word = random.choice(words)
+    logging.debug('Returning word ' + word)
     return word
 
 
 def get_medium_word():
     logging.debug('Getting an medium word')
-    # words = read_word_list('medium.txt')
-    words = MEDIUM_WORDS
-    word = str(random.choice(words)).lower()
-    logging.debug('Returning word {}'.format(word))
+    words = get_word_list('medium.txt')
+    word = random.choice(words)
+    logging.debug('Returning word ' + word)
     return word
 
 
 def get_hard_word():
     logging.debug('Getting an hard word')
-    # words = read_word_list('hard.txt')
-    words = HARD_WORDS
-    word = str(random.choice(words)).lower()
-    logging.debug('Returning word {}'.format(word))
+    words = get_word_list('hard.txt')
+    word = random.choice(words)
+    logging.debug('Returning word ' + word)
     return word
 
 
@@ -147,9 +140,9 @@ def get_man(turn_counter):
     return man
 
 
-def check_if_lost(game_variables):
-    if len(game_variables['letters']) >= 12:
-        man = get_man(game_variables['counter'])
+def check_if_lost(words):
+    if len(words['letters']) >= 12:
+        man = get_man(words['counter'])
         print_gallows(man)
         print('\n* * * * * * * * * *\n You lost! \n* * * * * * * * * *\n')
         time.sleep(5)
@@ -158,8 +151,8 @@ def check_if_lost(game_variables):
         return True
 
 
-def check_if_won(game_variables):
-    if '-' not in game_variables['blanked']:
+def check_if_won(words):
+    if '-' not in words['blanked']:
         logging.debug('No more letters to guess')
         print('\n* * * * * * * * * *\n You win, congratulations! \n* * * * * * * * * *\n')
         time.sleep(3)
@@ -184,7 +177,7 @@ def play_game():
         elif difficulty == '1' or '2' or '3':
             logging.debug('Difficulty is a valid choice')
             word = get_word(difficulty)
-            game_variables = blank_word(word)
+            words = blank_word(word)
         else:
             print('That was not a valid choice')
 
@@ -193,17 +186,17 @@ def play_game():
         while win_flag:
 
             print('\n* * * * * * * * * *\n    Hangman    \n* * * * * * * * * *\n' +
-                  game_variables['blanked'] + '\n' + 'Guesses:' + game_variables['letters'])
-            man = get_man(game_variables['counter'])
+                  words['blanked'] + '\n' + 'Guesses:' + words['letters'])
+            man = get_man(words['counter'])
             print_gallows(man)
             guess = input('Make a guess: ')
             if len(guess) == 1:
-                game_variables = guess_letter(game_variables, guess)
+                words = guess_letter(words, guess)
             else:
                 print('Only guess one letter at a time')
 
-            win_flag = check_if_won(game_variables)
-            win_flag = check_if_lost(game_variables)
+            win_flag = check_if_won(words)
+            win_flag = check_if_lost(words)
 
 
 if __name__ == "__main__":
