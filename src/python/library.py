6945f3285141611b798b3108b66b7ac0452c85ef
Jamie Coupe <Jamie.coupe@sky.com>
2019-05-30 14:08:05 +0100

    Update library.py

--- src/python/library.py
+++ src/python/library.py
@@ -6,25 +6,29 @@ import time
 from src.python.games import naughtsandcrosses
 from src.python.games import hangman
 
-while True:
-    print('Available games:\n'
-          '1. Naughts and Crosses\n'
-          '2. Hangman')
+def main():
+  while True:
+      print('Available games:\n'
+            '1. Naughts and Crosses\n'
+            '2. Hangman')
 
-    game = input('Which game would you like to play?').lower()
+      game = input('Which game would you like to play?').lower()
 
-    if game == 'q':
-        print('Quitting')
-        break
-    elif game == '1':
-        os.system('clear')
-        naughtsandcrosses.play_game()
-    elif game == '2':
-        os.system('clear')
-        hangman.play_game()
-    else:
-        print('Please enter a valid game number')
+      if game == 'q':
+          print('Quitting')
+          break
+      elif game == '1':
+          os.system('clear')
+          naughtsandcrosses.play_game()
+      elif game == '2':
+          os.system('clear')
+          hangman.play_game()
+      else:
+          print('Please enter a valid game number')
 
-time.sleep(3)
-os.system('clear')
-exit()
+  time.sleep(3)
+  os.system('clear')
+  exit()
+
+if __name__ == "__main__":  
+  main()
