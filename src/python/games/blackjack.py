379c9cdabd6b5ecf8f172ea5efd2d323a01d140b
Jamie Coupe <Jamie.coupe@sky.com>
2019-05-30 14:08:47 +0100

    Added game into function

--- src/python/games/blackjack.py
+++ src/python/games/blackjack.py
@@ -4,17 +4,23 @@
 import logging
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s -  %(levelname)s -  %(message)s')
-logging.disable(logging.DEBUG)
+# logging.disable(logging.DEBUG)
 
-while True:
-    logging.debug('Blackjack has started')
 
-    print('\n* * * * * * * * * *\n    Blackjack    \n* * * * * * * * * *\n')
-    action = input('Make an action: T twist, S stick').lower()
-    logging.debug('Action choosen was {}'.format(action))
+def play_game():
+  while True:
+      logging.debug('Blackjack has started')
 
-    if action == 'qq':
-        break
-    elif action == 't':
-        draw_hand(hand)
-        print(calculate_score)
+      print('\n* * * * * * * * * *\n    Blackjack    \n* * * * * * * * * *\n')
+      action = input('Make an action: T twist, S stick').lower()
+      logging.debug('Action choosen was {}'.format(action))
+
+      if action == 'qq':
+          break
+      elif action == 't':
+          draw_hand(hand)
+          print(calculate_score)
+
+
+if __name__ == "__main__":
+    play_game()
