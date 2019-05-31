7238dca3b733a9ac8e0bae5f0425e3f5eb9e4477
Jamie Coupe <Jamie.coupe@sky.com>
2019-05-31 11:33:39 +0100

    Reindent using 4 spaces

--- src/python/games/blackjack.py
+++ src/python/games/blackjack.py
@@ -9,137 +9,137 @@ logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s -  %(levelname)s -
 
 
 def generate_deck():
-  hearts = [card for card in range(1, 14)]
-  diamonds = [card for card in range(1, 14)]
-  clubs = [card for card in range(1, 14)]
-  spades = [card for card in range(1, 14)]
-  deck = hearts + diamonds + clubs + spades
-  random.shuffle(deck)
-  return deck
+    hearts = [card for card in range(1, 14)]
+    diamonds = [card for card in range(1, 14)]
+    clubs = [card for card in range(1, 14)]
+    spades = [card for card in range(1, 14)]
+    deck = hearts + diamonds + clubs + spades
+    random.shuffle(deck)
+    return deck
 
 
 def deal_hands(deck):
-  hands = [[deck.pop(), deck.pop()], [deck.pop(), deck.pop()]]
-  return hands
+    hands = [[deck.pop(), deck.pop()], [deck.pop(), deck.pop()]]
+    return hands
 
 
 def translate_hand(hand):
-  translated_hand = []
-  for card in hand:
-    if card == 1:
-      translated_hand.append('Ace')
-    elif card == 11:
-      translated_hand.append('Jack')
-    elif card == 12:
-      translated_hand.append('Queen')
-    elif card == 13:
-      translated_hand.append('King')
-    else:
-      translated_hand.append(card)
+    translated_hand = []
+    for card in hand:
+        if card == 1:
+            translated_hand.append('Ace')
+        elif card == 11:
+            translated_hand.append('Jack')
+        elif card == 12:
+            translated_hand.append('Queen')
+        elif card == 13:
+            translated_hand.append('King')
+        else:
+            translated_hand.append(card)
 
-  return translated_hand
+    return translated_hand
 
 
 def calculate_total(hand):
-  total = 0
-  for card in hand:
-    if card > 10:
-      total += 10
-    else:
-      total += card
-  return total
+    total = 0
+    for card in hand:
+        if card > 10:
+            total += 10
+        else:
+            total += card
+    return total
 
 
 def twist(deck, hand):
-  hand.append(deck.pop())
-  return hand
+    hand.append(deck.pop())
+    return hand
 
 
 def check_loss(hand):
-  if calculate_total(hand) > 21:
-    return True
-  else:
-    return False
+    if calculate_total(hand) > 21:
+        return True
+    else:
+        return False
 
 
 def players_turn(deck, hand):
-  while True:
-    print('\n* * * * * * * * * *\n    Blackjack    \n* * * * * * * * * *\n')
-    print("Your hand is: {}. Total: {}".format(translate_hand(hand), calculate_total(hand)))
+    while True:
+        print('\n* * * * * * * * * *\n    Blackjack    \n* * * * * * * * * *\n')
+        print("Your hand is: {}. Total: {}".format(translate_hand(hand), calculate_total(hand)))
 
-    if check_loss(hand):
-      break
+        if check_loss(hand):
+            break
 
-    action = input('Make an action: T twist, S stand \n').lower()
-    logging.debug('Action chosen was {}'.format(action))
-    if action == 'qq':
-      return 'quit'
-    elif action == 't':
-      hand = twist(deck, hand)
-    elif action == 's':
-      return hand
-    else:
-      print("Please enter a valid option")
-  return hand
+        action = input('Make an action: T twist, S stand \n').lower()
+        logging.debug('Action chosen was {}'.format(action))
+        if action == 'qq':
+            return 'quit'
+        elif action == 't':
+            hand = twist(deck, hand)
+        elif action == 's':
+            return hand
+        else:
+            print("Please enter a valid option")
+    return hand
 
 
 def dealers_turn(deck, hand):
-  while True:
-    total = calculate_total(hand)
+    while True:
+        total = calculate_total(hand)
 
-    if check_loss(hand):
-      break
-    if total < 16:
-      print('Dealer is twisting')
-      hand = twist(deck, hand)
-      time.sleep(1)
-    else:
-      print('Dealer has stood')
-      time.sleep(1)
-      return hand
-  return hand
+        if check_loss(hand):
+            break
+        if total < 16:
+            print('Dealer is twisting')
+            hand = twist(deck, hand)
+            time.sleep(1)
+        else:
+            print('Dealer has stood')
+            time.sleep(1)
+            return hand
+    return hand
 
 
 def get_winner(player_hand, dealer_hand):
-  player_total = calculate_total(player_hand)
-  dealer_total = calculate_total(dealer_hand)
+    player_total = calculate_total(player_hand)
+    dealer_total = calculate_total(dealer_hand)
 
-  if player_total > dealer_total:
-    print('Player wins with {}!'.format(player_total))
-  elif player_total < dealer_total:
-    print('Dealer wins with {}!'.format(dealer_total))
-  else:
-    print('It was a draw!\nPlayer had: {}\nDealer had: {}'.format(player_total, dealer_total))
+    if player_total > dealer_total:
+        print('Player wins with {}!'.format(player_total))
+    elif player_total < dealer_total:
+        print('Dealer wins with {}!'.format(dealer_total))
+    else:
+        print('It was a draw!\nPlayer had: {}\nDealer had: {}'.format(player_total, dealer_total))
 
 
 def play_game():
-  while True:
-    logging.debug('Blackjack has started')
-    deck = generate_deck()
-    hands = deal_hands(deck)
-    player_hand = hands[0]
-    dealer_hand = hands[1]
+    while True:
+        logging.debug('Blackjack has started')
+        deck = generate_deck()
+        hands = deal_hands(deck)
+        player_hand = hands[0]
+        dealer_hand = hands[1]
 
-    player_hand = players_turn(deck, player_hand)
-    if player_hand == 'quit':
-      logging.debug('Quitting the game')
-      print('Returning to main library')
-      time.sleep(3)
-      break
+        player_hand = players_turn(deck, player_hand)
+        if player_hand == 'quit':
+            logging.debug('Quitting the game')
+            print('Returning to main library')
+            time.sleep(3)
+            break
 
-    if check_loss(player_hand):
-      logging.debug('Player lost')
-      print("You Lose you went higher than 21.\n")
-      break
+        if check_loss(player_hand):
+            logging.debug('Player lost')
+            print("You Lose you went higher than 21.\n")
+            break
 
-    dealer_hand = dealers_turn(deck, dealer_hand)
-    if check_loss(dealer_hand):
-      logging.debug('Dealer lost')
-      print("Dealer has lost they higher than 21.\n")
-      break
+        dealer_hand = dealers_turn(deck, dealer_hand)
+        if check_loss(dealer_hand):
+            logging.debug('Dealer lost')
+            print("Dealer has lost they higher than 21.\n")
+            break
 
-    get_winner(player_hand, dealer_hand)
-    time.sleep(3)
+        get_winner(player_hand, dealer_hand)
+        time.sleep(3)
 
 
 if __name__ == "__main__":
